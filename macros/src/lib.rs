1a66a81b0aaabc8a594b1d5195f21189d86ded7c
JohnMitchell04 <johnrpm1@gmail.com>
2024-07-19 14:56:38 +0100

    Usability improvements

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,46 +1,52 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Ident};
 
 // TODO: Add the ability for compile time checks to prevent the user from trying to create a struct from invalid JSON
 #[proc_macro_derive(Deserialise)]
 pub fn derive_deserialise(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = ast.ident;
-    let data = ast.data;
 
-    if let Data::Struct(data) = data {
-        let mut fields_vals = quote!();
-
-        let fields: Fields = data.fields;
-        if let Fields::Named(named) = fields {
-            for field in named.named {
-                let name = field.ident.unwrap();
-                
-                fields_vals.extend(quote!(
-                    #name: value
-                        .remove(stringify!(#name))
-                        .unwrap()
-                        .try_into()
-                        .unwrap(),
-                ));
-            }
-        } else {
-            unimplemented!()
-        }
-
-        let output = quote! {
-            impl Deserialise for #name {
-                fn from_str(input: &str) -> Self {
-                    let mut value = jsoxiden::from_str(input).unwrap();
-    
-                    Self { #fields_vals }
-                }
-            }
-        };
-    
-        proc_macro::TokenStream::from(output)
-    } else {
-        unimplemented!()
+    match ast.data {
+        Data::Struct(data) => generate_deserialise_impl(name, data),
+        _ => {
+            quote! {
+                compile_error!("Deserialise can only be derived for structs with named fields.")
+            }.into()
+        },
     }
+}
+
+fn generate_deserialise_impl(name: Ident, data: DataStruct) -> TokenStream {
+    let fields = if let Fields::Named(fields) = data.fields {
+        fields.named
+    } else {
+        return quote! {
+            compile_error!("Deserialise can only be derived for structs with named fields.");
+        }.into();
+    };
+
+    let fields_vals = fields.into_iter().filter_map(|field| {
+        let field_name = field.ident.unwrap();
+        quote! {
+            #field_name:
+                value.remove(stringify!(#field_name))
+                .ok_or_else(|| format!("Missing field: {}", stringify!(#field_name)))?
+                .try_into()
+                .map_err(|_| format!("Failed to parse field: {}", stringify!(#field_name)))?
+        }.into()
+    });
+
+    let output = quote! {
+        impl Deserialise for #name {
+            fn from_str(input: &str) -> Result<Self, jsoxiden::ParseError> {
+                let mut value = jsoxiden::from_str(input)?;
+
+                Ok(Self { #(#fields_vals),* })
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(output)
 }
